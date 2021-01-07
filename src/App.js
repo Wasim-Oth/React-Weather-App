5853b843ed64517daf732b79f033d6b6b65d201d
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2021-01-07 17:08:11 +0100

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CityWeather from "./componenets/cities";
+import Forecast from "./componenets/forecast";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <h1 id="weather">Weather</h1>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={CityWeather} />
+          <Route path="/:cityId" component={Forecast} />
+        </Switch>
+      </Router>
     </div>
   );
 }
